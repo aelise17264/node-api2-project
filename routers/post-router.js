00c6823e4645735b2e6532f1f3f6e884db74d4e0
unknown <aslies17264@gmail.com>
2020-09-29 13:32:03 -0600

    can add a post to api

--- routers/post-router.js
+++ routers/post-router.js
@@ -4,6 +4,8 @@ const router = express.Router()
 
 const Posts = require('../data/db')
 
+let nextId = 9;
+
 router.get('/', (req, res) => {
     Posts.find(req.query)
     .then(post => {
@@ -12,6 +14,21 @@ router.get('/', (req, res) => {
     .catch(error => {
         console.log(error)
         res.status(500).json({ message: 'Error retrieving posts'})
+    })
+})
+
+router.post('/', (req, res) => {
+    const {title, contents} = req.body;
+    
+    Posts.insert({title, contents})
+    .then(post => {
+        post.id = nextId++;
+        res.status(201).json({message: 'New Post'})
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({message: 'Error adding post'
+        })
     })
 })
 
